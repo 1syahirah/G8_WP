f55095704641d7d2da1c37e0169092892086c167
SyukriOthman <syukriothman577@gmail.com>
2025-05-01 18:37:07 +0800

    added login functionality

--- js/login.js
+++ js/login.js
@@ -1,3 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const form = document.getElementById("login-form")
+    const form = document.getElementById("login-form");
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault(); // stop page from reloading
+
+        const email = form.email.value.trim();
+        const password = form.password.value;
+
+        if (!email || !password){
+            alert("Please enter both email and password.");
+            return;
+        }
+
+        //Get user form local storage, if no data just assign empty string
+        const users = JSON.parse(localStorage.getItem("users")) || [];
+
+        //find user with email
+        const foundUser = users.find(user => user.email == email);
+
+        //user not found
+        if (!foundUser){
+            alert("Usesr not found. Please register first.");
+            return;
+        }
+
+        //wrong password
+        if (foundUser.password != password) {
+            alert("Incorrect password.");
+            return;
+        }
+
+        //Stimulate login : store user session, so that we know who is currently logged in
+        localStorage.setItem("loggedInUser", JSON.stringify(foundUser));
+
+        alert("Login successful!");
+        window.location.href = "index.html"; // redirect to homepage
+
+    });
 });
